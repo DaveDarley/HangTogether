e88aea59698775758723530b404ef6536139c53f
davito <jdavedarley@gmail.com>
2022-03-17 11:09:29 -0400

    Update Display Friends
    tinder like swipe display for potential friends(not working like expected)

--- HangTogether/HangTogether/DisplayPotentialFriends.xaml.cs
+++ HangTogether/HangTogether/DisplayPotentialFriends.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,34 @@ namespace HangTogether
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DisplayPotentialFriends : ContentPage
     {
+        public ObservableCollection<UserProfile> _Profiles = new ObservableCollection<UserProfile>();
         public DisplayPotentialFriends()
         {
             InitializeComponent();
+        }
+
+        public void CardBinding()
+        {
+            _Profiles.Add(new UserProfile(FirestName = "Dave", LastName = ));
+        }
+
+        public ObservableCollection<UserProfile> Profile
+        {
+            get => _Profiles;
+            set
+            {
+                _Profiles = value;
+            }
+
+
+        }
+
+        public class UserProfile
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Age { get; set; }
+            public string Anecdote { get; set; }
         }
     }
 }
